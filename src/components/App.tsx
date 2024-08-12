5c6c70d84f56fc6af6abcab27b5e131adad2811b
Anil Pak <anil.pak@performio.co>
2024-08-12 13:27:14 +1000

    Add Chakra UI for styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const App: React.FC = () => {
-  return <div>Hello, Robot Simulator!</div>;
+  return (
+    <ChakraProvider>
+      <div>
+        <h1>Hello, Bellroy - Robot Simulator!</h1>
+      </div>
+    </ChakraProvider>
+  );
 };
 
 export default App;
